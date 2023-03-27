131eccf9b4c9223a30ab1907ff19d4f29ee4a30a
Aidan <aidanreynolds715@gmail.com>
2023-03-26 20:32:02 -0400

    debugging

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,18 +44,17 @@ export const SAVE_BOOK = gql`
 
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: ID!) {
-        removeBook(bookId: $bookId) {
-            _id
-            username
-            email
-            savedBooks {
-                bookId
-                authors
-                description
-                title
-                image
-                link
-            }
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
         }
+    
     }
 `;
